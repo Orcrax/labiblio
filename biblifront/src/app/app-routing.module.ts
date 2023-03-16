1a4b4318bc91f51f95cff15111d9ddc1c5d3d73c
Orcrax <matthieu.rebate@gmail.com>
2023-03-16 14:50:56 +0100

    search page

--- biblifront/src/app/app-routing.module.ts
+++ biblifront/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { BookmarksPageComponent } from './pages/bookmarks-page/bookmarks-page.component';
+import { ForthcomingPageComponent } from './pages/forthcoming-page/forthcoming-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { SelectedBookComponent } from './pages/selected-book/selected-book.component';
+import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
 import { SubscribePageComponent } from './pages/subscribe-page/subscribe-page.component';
 
 const routes: Routes = [
@@ -24,10 +29,25 @@ const routes: Routes = [
     component: ProfilePageComponent
   },
   {
-    path: 'navigationBar',
-    component: NavigationBarComponent
+    path: 'search',
+    component: SearchPageComponent
   },
-  
+  {
+    path: 'bookmarks',
+    component: BookmarksPageComponent
+  },
+  {
+    path: 'forthcoming',
+    component: ForthcomingPageComponent
+  },
+  {
+    path: 'selectedbook',
+    component: SelectedBookComponent
+  },
+  {
+    path: 'settings',
+    component: SettingsPageComponent
+  }
 ];
 
 @NgModule({
