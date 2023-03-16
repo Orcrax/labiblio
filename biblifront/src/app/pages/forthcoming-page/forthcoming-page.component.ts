efd096ad654eab8f6b0dbc30b4f9b8e72c21c6b0
Orcrax <matthieu.rebate@gmail.com>
2023-03-16 14:02:46 +0100

    creating profile page

---
+++ biblifront/src/app/pages/forthcoming-page/forthcoming-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forthcoming-page',
+  templateUrl: './forthcoming-page.component.html',
+  styleUrls: ['./forthcoming-page.component.scss']
+})
+export class ForthcomingPageComponent {
+
+}
